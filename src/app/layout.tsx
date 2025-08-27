6c21214fcbcf2f9c447a0b48ac35c40cb59f5529
webdotcokr <webdotcokr@gmail.com>
2025-08-27 10:46:24 +0900

    clarity 설치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -111,6 +111,17 @@ export default function RootLayout({
             gtag('config', 'G-CMVG6YV9XT');
           `}
         </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "t15h5rn6h7");
+          `}
+        </Script>
         
         <AuthProvider>
           <ConditionalLayout>
